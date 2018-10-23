12efb09aa8c57603bc043b35edd0fbad242e9b54
Dorieke Berends <dorieke90@gmail.com>
2018-10-23 15:29:51 +0200

    Added a form to make an ad

--- php sections/placead.php
+++ php sections/placead.php
@@ -1,3 +1,9 @@
 <div class="outer" id="place-ad">
-    There will be a form here that you can use to place an ad.
+    <p><strong>This form is made by a database student. Please don't enter sensitive information as protection of your data is not guaranteed!</strong> Try entering some fake data instead. </p>
+    <form method="post" action="adplaced.php">
+        <p><input name="title" type="text" placeholder="Ad title"></p>
+        <p><textarea name="description" type="text" placeholder="Ad description"></textarea></p>
+        <p><input name="price" type="number" step="0.01" placeholder="Price"></p>
+        <p><input name="submit" type="submit"></p>
+    </form>
 </div>
--- php sections/registerform.php
+++ php sections/registerform.php
@@ -6,7 +6,6 @@
         <p><input name="username" type="text" placeholder="Your username..."></p>
         <p><input name="email" type="email" placeholder="Your email..."></p>
         <p><input name="newpassword" type="text" placeholder="Your password..."></p>
-        <!-- <p><input name="newpassword2" type="text" placeholder="Confirm your password..."></p> -->
         <p><input name="submit" type="submit"></p>
     </form>
 </div>
