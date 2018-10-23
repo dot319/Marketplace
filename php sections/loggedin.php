6ae7482096b7bccb2a9fe5eba9b905d0303c7280
Dorieke Berends <dorieke90@gmail.com>
2018-10-23 12:57:23 +0200

    linked login form to users table in database

--- php sections/loggedin.php
+++ php sections/loggedin.php
@@ -1,3 +1,26 @@
 <div class="outer" id="loggedin">
     You clicked the login button.<br />
+
+<?php
+
+include 'php sections/connecttodatabase.php';
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$myQuery = "SELECT `Username`, `Email`, `Password` FROM users WHERE email = '$email'";
+$result = $conn->query($myQuery);
+
+if ($result->num_rows == 0) {
+    echo("Sorry, we don't know that email address!");
+}
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo("Your username is: " . $row["Username"] . ".<br />");
+    }
+}
+
+?>
+
 </div>
