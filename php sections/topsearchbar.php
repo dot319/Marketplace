bee50fb3167217e4376fb75b436d400a9f6a101f
Dorieke Berends <dorieke90@gmail.com>
2018-10-23 09:20:30 +0200

    styled top search bar on registration page

--- php sections/topsearchbar.php
+++ php sections/topsearchbar.php
@@ -1,6 +1,6 @@
-<div class="outer">
+<div class="outer" id="top-searchbar">
     <form>
         <input id="searchbar-itself" type="text" placeholder="Search for products to buy...">
-        <input id="searchbutton-itself" type="submit" value="Go!>"
+        <input id="searchbutton-itself" type="submit" value="Go!">
     </form>
 </div>
