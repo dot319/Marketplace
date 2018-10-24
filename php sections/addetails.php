ce5ec70890ce3be905fc38b7d3d2a88fc56e2b7b
Dorieke Berends <dorieke90@gmail.com>
2018-10-24 13:34:15 +0200

    Ad detail page now displays details about the ad that was clicked

--- php sections/addetails.php
+++ php sections/addetails.php
@@ -1,17 +1,42 @@
+<?php
+
+include 'php sections/connecttodatabase.php';
+
+$AdID = $_GET['AdID'];
+
+$myQuery = "SELECT * FROM Ads WHERE AdID='$AdID'";
+$myResult = $conn->query($myQuery);
+
+while ($row = $myResult->fetch_assoc()) {
+    $title = $row['Title'];
+    $description = $row['Description'];
+    $price = $row['Price'];
+    $seller = $row['Username'];
+}
+
+?>
+
 <div class="outer" id="ad-details">
     <div id="ad-details-wrapper">
         <div id="ad-details-header">
-            <h2>Title goes here</h2>
+            <h2><?php echo($title); ?></h2>
         </div>
         <div id="ad-details-description">
-            Description goes here
+            <?php echo($description); ?>
         </div>
         <div id="ad-details-price-seller">
-            <div id="ad-details-price">Price</div>
-            <div id="ad-details-seller">Seller name</div>
+            <div id="ad-details-price"><?php echo($price); ?></div>
+            <div id="ad-details-seller"><?php echo($seller); ?></div>
         </div>
         <div id="ad-details-contact-form">
-            Contact form goes here
+            <form method="post" action="messagesent.php">
+                <p><textarea name="message" placeholder="Write a message to the seller."></textarea></p>
+                <p><input type="submit" value="Send the seller a message!"></p>
+            </form>
         </div>
     </div>
 </div>
+
+<?php
+$conn->close();
+?>
