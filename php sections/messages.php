d1c6aa6320e3c26eb58769e7c3a5e3530d37d5e9
Dorieke Berends <dorieke90@gmail.com>
2018-10-24 15:18:19 +0200

    When logged in you can view all messages you sent or have been sent in the messages tab

--- php sections/messages.php
+++ php sections/messages.php
@@ -1,3 +1,26 @@
 <div class="outer" id="messages">
-Messages will be displayed here.
+Messages will be displayed here.<br />
+
+<?php
+
+include 'php sections/connecttodatabase.php';
+
+$UserID = $_SESSION['UserID'];
+
+$myQuery = "SELECT * FROM messages
+WHERE ReceiverID=$UserID OR SenderID=$UserID";
+$myResult = $conn->query($myQuery);
+while ($row = $myResult->fetch_assoc()) {
+    echo("<div class='message-item'>
+    <p><b>Sender Id:</b> " . $row['SenderID'] . "</p>
+    <p><b>Receiver Id:</b> " . $row['ReceiverID'] . "</p>
+    <p><b>Ad Id:</b> " . $row['AdID'] . "</p>
+    <p><b>Message:</b> " . $row['Message'] . "</p>
+    </div>");
+}
+
+?>
+
+<?php $conn->close(); ?>
+
 </div>
