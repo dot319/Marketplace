b291b05d0d00b0e8bb5d55348158ca5f192c89e1
Dorieke Berends <dorieke90@gmail.com>
2018-10-29 10:57:48 +0100

    When placing an ad you can select a category

--- php sections/adplaced.php
+++ php sections/adplaced.php
@@ -1,3 +1,11 @@
+<?php 
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+/*** THIS! ***/
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+?>
+
 <div class="outer" id="ad-placed">
     You clicked the "place ad" button!
 
@@ -8,10 +16,11 @@ include 'php sections/connecttodatabase.php';
 $title = $_POST['title'];
 $description = $_POST['description'];
 $price = $_POST['price'];
+$category = $_POST['category'];
 $username = $_SESSION['Username'];
 
-$myQuery = "INSERT INTO ads(`title`, `description`, `price`, `username`)
-VALUES ('$title', '$description', '$price', '$username')";
+$myQuery = "INSERT INTO ads(`title`, `description`, `price`, `catid`, `username`)
+VALUES ('$title', '$description', '$price', '$category', '$username')";
 
 if ($conn->query($myQuery) === TRUE) {
     echo("Ad placed successfully!");
--- php sections/placead.php
+++ php sections/placead.php
@@ -8,6 +8,26 @@
             <p><input name="title" type="text" placeholder="Ad title"></p>
             <p><textarea name="description" type="text" placeholder="Ad description"></textarea></p>
             <p><input name="price" type="number" step="0.01" placeholder="Price"></p>
+            <p><select name="category" placeholder="category">
+                <option value="1">Art</option>
+                <option value="2">Bicycles and mopeds</option>
+                <option value="3">Books</option>
+                <option value="4">Cars</option>
+                <option value="5">Children and babies</option>
+                <option value="6">Clothing and shoes</option>
+                <option value="7">Computers and laptops</option>
+                <option value="8">Gaming</option>
+                <option value="9">Garden</option>
+                <option value="10">Home</option>
+                <option value="11">Jewelry and bags</option>
+                <option value="12">Motors</option>
+                <option value="13">Music and instruments</option>
+                <option value="14">Pets and supplies</option>
+                <option value="18">Phones and tablets</option>
+                <option value="15">Sports</option>
+                <option value="16">Tickets</option>
+                <option value="17">Other</option>
+            </select></p>
             <p><input name="submit" type="submit" value="Place ad"></p>
         </form>
     </div> 
