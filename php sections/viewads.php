a7366fbdd397d63ac20b64bd90d921bbe1d0e9ae
Dorieke Berends <dorieke90@gmail.com>
2018-10-29 12:08:41 +0100

    When browsing ads you can now filter by category

--- php sections/viewads.php
+++ php sections/viewads.php
@@ -1,6 +1,6 @@
 <div class="outer" id="view-ads">
-
     See ads placed by users below:<br />
+    <br />
 
 <?php
 
@@ -13,7 +13,48 @@ if (isset($_POST['searchquery'])) {
     $mySearchQuery = "";
 }
 
-$myQuery = "SELECT * FROM ads " . $mySearchQuery . "ORDER BY AdID DESC LIMIT 50";
+if (isset($_POST['category'])) {
+    $category = $_POST['category'];
+    $myCatQuery = "AND CatId = '$category' ";
+    if ($mySearchQuery == "") {
+        $myCatQuery = "WHERE CatId = '$category' ";
+    }
+} else {
+    $myCatQuery = "";
+}
+
+?>
+
+    <form method="post" action="viewads.php">
+        <p>Category: <select name="category" placeholder="category">
+            <option value="1">Art</option>
+            <option value="2">Bicycles and mopeds</option>
+            <option value="3">Books</option>
+            <option value="4">Cars</option>
+            <option value="5">Children and babies</option>
+            <option value="6">Clothing and shoes</option>
+            <option value="7">Computers and laptops</option>
+            <option value="8">Gaming</option>
+            <option value="9">Garden</option>
+            <option value="10">Home</option>
+            <option value="11">Jewelry and bags</option>
+            <option value="12">Motors</option>
+            <option value="13">Music and instruments</option>
+            <option value="14">Pets and supplies</option>
+            <option value="18">Phones and tablets</option>
+            <option value="15">Sports</option>
+            <option value="16">Tickets</option>
+            <option value="17">Other</option>
+        </select>
+<?php if (isset($searchQuery)) { ?>
+        <input type="hidden" name="searchquery" value="<?php echo($searchQuery); ?>">
+<?php } ?>
+        <input type="submit" value="Filter results"></p>
+    </form>
+
+<?php
+
+$myQuery = "SELECT * FROM ads " . $mySearchQuery . $myCatQuery . "ORDER BY AdID DESC LIMIT 50";
 $myResult = $conn->query($myQuery);
 
 while ($row = $myResult->fetch_assoc()) {
