5527f93531362d458ebb18176976f6e777e28572
Dorieke Berends <dorieke90@gmail.com>
2018-10-29 12:29:30 +0100

    Profile page shows a list of ads placed by user

--- php sections/viewownprofile.php
+++ php sections/viewownprofile.php
@@ -1,3 +1,26 @@
+<?php include 'php sections/connecttodatabase.php' ?>
+
 <div class="outer" id="own-profile">
     Here you will be able to see your own profile and edit it.
+
+    <div id="own-profile-ads">
+        <h3>Ads you placed:</h3>
+<?php
+
+$username = $_SESSION['Username'];
+$myQuery = "SELECT * FROM ads WHERE Username = '$username'";
+$myResult = $conn->query($myQuery);
+
+if ($myResult->num_rows == 0) {
+    echo("You haven't placed any ads yet.");
+} else {
+    while ($row = $myResult->fetch_assoc()) { 
+        echo($row['Title'] . "<br />");
+    }
+}
+
+$conn->close();
+
+?>
+    </div>
 </div>
